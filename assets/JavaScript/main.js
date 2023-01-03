6ce1f894816439c1fa6d0610bfcd3692d5b742ec
CatalinBarbu <121017880+CatalinBarbu@users.noreply.github.com>
2023-01-03 14:26:10 +0100

    First Commit

---
+++ assets/JavaScript/main.js
@@ -0,0 +1,7 @@
+let myText = "<p>Hello again</p>";
+document.write(myText);
+const vorName = "Catalin";
+const nachName = "Barbu";
+document.write(
+  "<p>Name: " + vorName + "<br>" + "Nachname: " + nachName + "</p>"
+);
